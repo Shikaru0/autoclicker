4e1ddd65be91cf28b6eed4041172a4b523c38a7f
Shikaru0 <games1logins1tech@gmail.com>
2026-09-08 08:41:36 +0200

    Config

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,22 @@ use std::thread;
 use evdev::{AttributeSet, EventType, InputEvent, Key};
 use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
 
+use config::Config;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!"); // just to test if it loads correctly
+
+    let settings = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .build()?;
+
+    let delay = settings.get_int("delay")?;
+
+    let key = match settings.get_string("key")?.as_str(){
+        "left" => Key::BTN_LEFT,
+        "right" => Key::BTN_RIGHT,
+        _ => return Err("Use 'left' or 'right' for key input.".into()),
+    };
 
     let mut keys = AttributeSet::<Key>::new();
     keys.insert(Key::BTN_LEFT);
@@ -23,8 +37,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Main loop
     loop{
-        click(&mut device, Key::BTN_LEFT)?;
-        thread::sleep(Duration::from_millis(500));
+        click(&mut device, key)?;
+        thread::sleep(Duration::from_millis(delay as u64));
     }
 }
 
